8ddfc1927e919162d3336b923089bfc32b5585c6
Hiran117 <hiranhansaka60@gmail.com>
2020-10-04 20:36:56 +0530

    create SnackBar views

---
+++ app/src/main/java/com/example/home/views/SnackBar.java
@@ -0,0 +1,21 @@
+package com.example.home.views;
+
+import android.content.Context;
+import android.view.View;
+
+import com.example.home.R;
+import com.google.android.material.snackbar.Snackbar;
+
+public class SnackBar
+{
+    public static void showCustomSnackBar(Context context,View root,String message)
+    {
+        //custom snackBar
+        Snackbar sb = Snackbar.make(root,message,Snackbar.LENGTH_LONG);
+
+        sb.setActionTextColor(context.getResources().getColor(R.color.white));
+        View sbView = sb.getView();
+        sbView.setBackgroundColor(context.getResources().getColor(R.color.colorPrimary));
+        sb.show();
+    }
+}
